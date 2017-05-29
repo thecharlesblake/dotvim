82f6bc30c8659687e4cfcfea2037dce2e9b2f944
Charlie Blake <thecharlesblake@live.co.uk>
2017-05-29 03:07:53 -0700

    Fill in missing setup file

--- vim-setup.bash
+++ vim-setup.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 ln -s ~/.vim/vimrc ~/.vimrc
+cd ~/.vim
+git submodule update --init
+git submodule foreach git pull origin master
+
+echo "
+function f () {
+    if [  !=  ] ; then
+        cd  && ls
+    else
+        ls
+    fi
+}
+" >> ~/.bashrc
+
